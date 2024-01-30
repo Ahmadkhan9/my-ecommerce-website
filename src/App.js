2b2c3eead7f0fb7af8508cd8c1b1282802b25a0d
AhmadKhan9 <mafriand77@gmail.com>
2024-01-31 00:52:49 +0500

    created categories and added saas file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 
-
+import './categories.style.scss'
 
 const App = () => {
+  const categories = [
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+  
   return (
-    <h1>Hello world</h1>
+    <div className="categories-container">
+      {categories.map(({id , title, imageUrl}) => {
+        return (<div key={id} className="category-container">
+        <div className="background-Image" style={{
+          backgroundImage : `url(${imageUrl})`
+        }}/>
+        <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>)
+      })}
+    </div>
   )
 }
 
